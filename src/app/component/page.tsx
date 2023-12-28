ac2ef99f6ed1d9dfebed70269bfa3e2ac64f850e
piyawatpm <piyawat_00101@hotmail.com>
2023-12-28 19:32:25 +1100

    initaial commit + navbar

---
+++ src/app/component/page.tsx
@@ -0,0 +1,8 @@
+const Component = () => {
+  return (
+    <div className=" w-screen h-screen bg-white flex items-center justify-center">
+      <div className=" w-10 h-10 bg-red-500"></div>
+    </div>
+  );
+};
+export default Component;
