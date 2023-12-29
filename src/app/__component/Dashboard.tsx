9e3301cd7f5994a5b03330b13fa5993f8fced245
piyawatpm <piyawat_00101@hotmail.com>
2023-12-29 17:35:06 +1100

    feat: add mock data for Dashboard

--- src/app/__component/Dashboard.tsx
+++ src/app/__component/Dashboard.tsx
@@ -1,5 +1,46 @@
 import Image from "next/image";
+import { useState } from "react";
+type WasherStep =
+  | "Detergent"
+  | "Softener"
+  | "Bleach"
+  | "INPUT 4"
+  | "INPUT 5"
+  | "INPUT 6";
 const Dashboard = () => {
+  const [washerStep, setWasherStep] = useState<WasherStep>("Detergent");
+  const mockPumpStatus = [1, 3, 4, 6];
+  const mockFluidLeve = [
+    {
+      name: "PUMP 1",
+      status: "FULL",
+    },
+    {
+      name: "PUMP 2",
+      status: "LOW",
+    },
+    {
+      name: "PUMP 3",
+      status: "FULL",
+    },
+    {
+      name: "PUMP 4",
+      status: "FULL",
+    },
+    {
+      name: "PUMP 5",
+      status: "LOW",
+    },
+    {
+      name: "PUMP 6",
+      status: "FULL",
+    },
+  ];
+  const mockWashCycleData = {
+    daily: 6,
+    weekly: 36,
+    monthly: 144,
+  };
   return (
     <div className=" w-full h-[1000px]  flex flex-col text-base gap-y-[4.75rem]">
       <div className=" flex flex-col gap-y-[.85rem]">
@@ -16,7 +57,11 @@ const Dashboard = () => {
             return (
               <div
                 key={index}
-                className="  py-2 w-[10rem] font-bold rounded-[.25rem] bg-[#F5F5F5] text-black flex items-center justify-center"
+                className={` ${
+                  washerStep === e
+                    ? "bg-[#06DE1C] text-white"
+                    : "bg-[#F5F5F5] text-black"
+                } py-2 w-[10rem] font-bold rounded-[.25rem]  flex items-center justify-center`}
               >
                 {e}
               </div>
@@ -46,17 +91,29 @@ const Dashboard = () => {
                   key={index}
                   className=" flex flex-col gap-y-[1.4rem] items-center"
                 >
-                  <div className=" py-2 w-[10rem] font-bold rounded-[.25rem] bg-[#F5F5F5] text-black flex items-center justify-center">
+                  <div
+                    className={`  ${
+                      mockPumpStatus.includes(index + 1)
+                        ? "bg-[#06DE1C] text-white"
+                        : "bg-[#F5F5F5] text-black"
+                    } py-2 w-[10rem] font-bold rounded-[.25rem] flex items-center justify-center`}
+                  >
                     {e}
                   </div>
-                  <Image
-                    className=""
-                    src="/pump.png"
-                    alt="pump"
-                    width={71}
-                    height={71}
-                    priority
-                  />
+                  <div className=" flex-1 flex items-center justify-center">
+                    {mockPumpStatus.includes(index + 1) ? (
+                      <Image
+                        className=""
+                        src="/pump.png"
+                        alt="pump"
+                        width={71}
+                        height={71}
+                        priority
+                      />
+                    ) : (
+                      <div className=" w-[1.2rem] h-[1.2rem] bg-black rounded-full"></div>
+                    )}
+                  </div>
                 </div>
               );
             }
@@ -66,41 +123,37 @@ const Dashboard = () => {
       <div className=" flex flex-col gap-y-[.85rem]">
         <h1 className="heading">FLUID LEVEL</h1>
         <div className=" flex gap-x-2">
-          {["PUMP 1", "PUMP 2", "PUMP 3", "PUMP 4", "PUMP 5", "PUMP 6"].map(
-            (e, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`${
-                    index % 2 == 0 ? "bg-[#FF0404]" : "bg-[#06DE1C]"
-                  } py-2 w-[10rem] gap-y-2 text-white flex-col  font-bold rounded-[.25rem] bg-[#F5F5F5] flex items-center justify-center`}
-                >
-                  <p>{e}</p>
-                  <p>FULL</p>
-                </div>
-              );
-            }
-          )}
+          {mockFluidLeve.map((e, index) => {
+            return (
+              <div
+                key={index}
+                className={`${
+                  e.status === "FULL" ? "bg-[#06DE1C]" : "bg-[#FF0404]"
+                } py-2 w-[10rem] gap-y-2 text-white flex-col  font-bold rounded-[.25rem]  flex items-center justify-center`}
+              >
+                <p>{e.name}</p>
+                <p>{e.status}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div className=" flex flex-col gap-y-[.85rem]">
         <h1 className=" heading">WASH CYCLE</h1>
         <div className=" flex gap-x-2">
-          {[
-            { tpye: "daily", value: 6 },
-            { tpye: "weekly", value: 36 },
-            { tpye: "monthly", value: 114 },
-          ].map((e, index) => {
-            return (
-              <div
-                key={index}
-                className={` py-2 w-[10rem] gap-y-2 text-black flex-col  font-bold rounded-[.25rem] bg-[#F5F5F5] flex items-center justify-center`}
-              >
-                <p className=" uppercase">{e.tpye}</p>
-                <p>{index}</p>
-              </div>
-            );
-          })}
+          {[{ tpye: "daily" }, { tpye: "weekly" }, { tpye: "monthly" }].map(
+            (e, index) => {
+              return (
+                <div
+                  key={index}
+                  className={` py-2 w-[10rem] gap-y-2 text-black flex-col  font-bold rounded-[.25rem] bg-[#F5F5F5] flex items-center justify-center`}
+                >
+                  <p className=" uppercase">{e.tpye}</p>
+                  <p>{mockWashCycleData[e.tpye]}</p>
+                </div>
+              );
+            }
+          )}
         </div>
       </div>
     </div>
