9eec6ffcc796b32200efe8931a6e2bdc7b573972
piyawatpm <piyawat_00101@hotmail.com>
2024-01-10 11:48:09 +1100

    test commit

--- src/app/_component/InputSetup.tsx
+++ src/app/_component/InputSetup.tsx
@@ -1,30 +1,50 @@
 import { Modal } from "antd";
 
 import { useState } from "react";
-import { useStatus } from "../_swr/useStatus";
+import { InputType, useStatus } from "../_swr/useStatus";
+import axios from "axios";
 
 const InputSetup = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedId, setSelectedId] = useState<string>("");
+  const initialInput = {
+    inputName: "",
+    inputMl: "",
+    inputId: null,
+  };
+  const [seletedInput, setSelectedInput] = useState(initialInput);
   const { data } = useStatus(false);
+
   const handleCloseModal = () => {
     setIsModalOpen(false);
-    setSelectedId("");
+    setSelectedInput(initialInput);
   };
   const handleOpenModal = () => {
     setIsModalOpen(true);
   };
-  const handleEditInput = async (id: string) => {
+  const handleEditInput = async (inputData: InputType) => {
     // call post edit api here
+    try {
+      const res = await axios.post(
+        `/api/input/${inputData.inputId}`,
+        inputData
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+    handleCloseModal();
   };
   return (
     <div className=" w-full h-[1000px]  flex flex-col text-base gap-y-[4.75rem]">
       <Modal
         title=""
         open={isModalOpen}
+        // onOk={handleOk}
         closeIcon={null}
+        // style={{ backgroundColor: 'transparent' }}
         centered
         footer={null}
+        // onCancel={handleCancel}
         className=" !w-[75rem] !"
         closable={false}
         onCancel={handleCloseModal}
@@ -36,7 +56,12 @@ const InputSetup = () => {
               <p className=" text-[1.2rem]">Input Name</p>
               <input
                 type="text"
-                placeholder="DETERGENT"
+                value={seletedInput.inputName}
+                onChange={(e) => {
+                  setSelectedInput((p) => {
+                    return { ...p, inputName: e.target.value };
+                  });
+                }}
                 className=" w-full pl-[1.65rem] py-4 bg-[#D9D9D9]"
               />
             </div>
@@ -44,7 +69,12 @@ const InputSetup = () => {
               <p className=" text-[1.2rem]">ml</p>
               <input
                 type="text"
-                placeholder={"1"}
+                value={seletedInput.inputMl}
+                onChange={(e) => {
+                  setSelectedInput((p) => {
+                    return { ...p, inputMl: e.target.value };
+                  });
+                }}
                 className=" w-full pl-[1.65rem] py-4 bg-[#D9D9D9]"
               />
             </div>
@@ -58,7 +88,7 @@ const InputSetup = () => {
             </button>
             <button
               onClick={() => {
-                handleEditInput(selectedId);
+                handleEditInput(seletedInput);
               }}
               className=" w-[8rem] h-[3.2rem] bg-black "
             >
@@ -70,51 +100,52 @@ const InputSetup = () => {
       <div className=" flex flex-col gap-y-[.85rem]">
         <h1 className=" heading">INPUT SETUP</h1>
         <div className=" flex gap-x-2">
-          {data?.inputStatus.map(
-            (input: { status: string; inputName: string; inputId: string }) => {
-              return (
-                <div
-                  key={input.inputId}
-                  className=" flex flex-col items-center gap-y-5"
-                >
-                  <div className=" flex flex-col items-center gap-y-2">
-                    {" "}
-                    <p className=" font-bold text-[1.2rem]">Input name</p>
-                    <div
-                      className={`  py-2 bg-[#F5F5F5] text-black w-[10rem] font-medium rounded-[.25rem]  flex flex-col items-center justify-center`}
-                    >
-                      <p>{input.inputName}</p>
-                    </div>
-                  </div>
-                  <div className=" flex flex-col items-center gap-y-2">
-                    <p className=" font-bold text-[1.2rem]">ml</p>
-                    <div
-                      className={`  py-2 bg-[#F5F5F5] text-black w-[10rem] font-medium rounded-[.25rem]  flex flex-col items-center justify-center`}
-                    >
-                      <p>{1}</p>
-                    </div>
-                  </div>
-                  <button
-                    onClick={handleOpenModal}
-                    className=" cursor-pointer gap-x-[.3rem] py-2 w-[10rem] font-bold rounded-[.25rem] button-primary text-black flex items-center justify-center"
+          {data?.inputStatus.map((input: InputType) => {
+            return (
+              <div
+                key={input.inputId}
+                className=" flex flex-col items-center gap-y-5"
+              >
+                <div className=" flex flex-col items-center gap-y-2">
+                  {" "}
+                  <p className=" font-bold text-[1.2rem]">Input name</p>
+                  <div
+                    className={`  py-2 bg-[#F5F5F5] text-black w-[10rem] font-medium rounded-[.25rem]  flex flex-col items-center justify-center`}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className=" w-[1.2rem] h-[1.2rem]"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                    >
-                      <path
-                        d="M11.55 3.9L20.1 12.45L8.55 24H0V15.45L11.55 3.9ZM11.55 8.25L3 16.8V21H7.2L15.75 12.45L11.55 8.25ZM15.6 0L24 8.55L21.45 11.1L12.9 2.55L15.6 0Z"
-                        fill="black"
-                      />
-                    </svg>
-                    <p>Edit</p>
-                  </button>
+                    <p>{input.inputName}</p>
+                  </div>
                 </div>
-              );
-            }
-          )}
+                <div className=" flex flex-col items-center gap-y-2">
+                  <p className=" font-bold text-[1.2rem]">ml</p>
+                  <div
+                    className={`  py-2 bg-[#F5F5F5] text-black w-[10rem] font-medium rounded-[.25rem]  flex flex-col items-center justify-center`}
+                  >
+                    <p>{input.inputMl}</p>
+                  </div>
+                </div>
+                <button
+                  onClick={() => {
+                    handleOpenModal();
+                    setSelectedInput(input);
+                  }}
+                  className=" cursor-pointer gap-x-[.3rem] py-2 w-[10rem] font-bold rounded-[.25rem] button-primary text-black flex items-center justify-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className=" w-[1.2rem] h-[1.2rem]"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <path
+                      d="M11.55 3.9L20.1 12.45L8.55 24H0V15.45L11.55 3.9ZM11.55 8.25L3 16.8V21H7.2L15.75 12.45L11.55 8.25ZM15.6 0L24 8.55L21.45 11.1L12.9 2.55L15.6 0Z"
+                      fill="black"
+                    />
+                  </svg>
+                  <p>Edit</p>
+                </button>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
